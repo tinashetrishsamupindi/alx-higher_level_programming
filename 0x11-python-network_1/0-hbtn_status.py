37e99fd948605a1fd1254c3b2f7dc5bc12002323
tinashetrishsamupindi <tinashetrishsamupindi@gmail.com>
2023-09-29 12:56:22 -0700

    network

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that fetches  https://alx-intranet.hbtn.io/status"""
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
+    print('\t- utf8 content: {}'.format(content.decode('utf8')))
